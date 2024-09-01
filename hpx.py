dbc5c8044dfad1f026b561c3f76744850198f71a
harrionparrix <varunpat44@gmail.com>
2024-09-01 18:20:21 +0530

    carpe diem

--- hpx.py
+++ hpx.py
@@ -1,15 +1,114 @@
-# Description: This file contains the strategies for the Rock-Paper-Scissors game from hpx. Make your own files.
+import random
+from collections import deque, defaultdict
+
 def HyperBeam(prev_play, opponent_name, opponent_history=["R","R"]): # hpx1
-    return "R"
+    opponent_history.append(prev_play)
+    if len(opponent_history) > 2:
+        opponent_history.pop(0)
+
+    guess = "R"
+    if opponent_history[-1] == "R":
+        guess = "P"
+    elif opponent_history[-1] == "P":
+        guess = "S"
+    elif opponent_history[-1] == "S":
+        guess = "R"
+
+    return guess
+
+
+def LRS(arr):
+    n = len(arr)
+    for size in range(5, 0, -1):
+        temp = arr[-size:]
+        for start in range(n - 2*size + 1):
+            if arr[start:start+size] == temp:
+                return temp
+    return []
 
 def EldenRing(prev_play, opponent_name, opponent_history=[], my_moves=[]): # hpx2
-    return "R"
+    moves = {'P': 'S', 'R': 'P', 'S': 'R'}
 
-def Maverick(prev_play, opponent_name, opponent_history=[]): # hpx3
-    return "R"
+    if prev_play:
+        opponent_history.append(prev_play)
+    
+    if len(opponent_history) < 10:
+        move = random.choice(["R", "P", "S"])
+        my_moves.append(move)
+        return move
+    
+    if len(opponent_history) > 10:
+        opponent_history.pop(0)
+
+    if len(my_moves) > 10:
+        my_moves.pop(0)
+    
+    pattern = LRS(opponent_history)
+
+    if pattern:
+        next_move = pattern[-1]
+        move = moves[next_move] 
+    else:
+        move = random.choice(["R", "P", "S"])
+
+    my_moves.append(move)
+    return move
+
+
+def Maverick(prev_play, opponent_name, opponent_history=[], play_order=defaultdict(int), repeat=3):
+    if not prev_play:
+        prev_play = random.choice(['R', 'P', 'S'])
+    opponent_history.append(prev_play)
+
+    if len(opponent_history) > repeat:
+        recent_sequence = "".join(opponent_history[-repeat:])
+        play_order[recent_sequence] += 1
+
+    if len(opponent_history) > repeat:
+        possible_next = ["".join(opponent_history[-repeat+1:]) + "R",
+                         "".join(opponent_history[-repeat+1:]) + "P",
+                         "".join(opponent_history[-repeat+1:]) + "S"]
+        prediction = max(possible_next, key=lambda seq: play_order[seq])[-1]
+    else:
+        prediction = random.choice(['R', 'P', 'S'])
+
+    ideal_response = {'P': 'S', 'R': 'P', 'S': 'R'}
+    return ideal_response[prediction]
 
 def DragonDance(prev_play, opponent_name, opponent_history=[]): # hpx4
-    return "R"
+    if prev_play:
+        opponent_history.append(prev_play)
+    
+    if prev_play == '':
+        prev_play = "R"
+    moves = {'P': 'S', 'R': 'P', 'S': 'R'}
+    return moves[prev_play]
+
 
 def SuperBaby(prev_play, opponent_name, opponent_history=[]):  # hpx5
-    return "R"
+    
+    if not prev_play:
+        prev_play = "S"
+
+    if opponent_name == "taylor_strategy":
+        if len(opponent_history) == 1:
+            return "S"
+        else:
+            moves = {'P': 'S', 'R': 'P', 'S': 'R'}
+            return moves[moves[prev_play]]
+
+    else:
+        opponent_history.append(prev_play)
+        
+        if len(opponent_history) > 2:
+            opponent_history.pop(0)
+
+        guess = "R"
+        if opponent_history[-1] == "R":
+            guess = "P"
+        elif opponent_history[-1] == "P":
+            guess = "S"
+        elif opponent_history[-1] == "S":
+            guess = "R"
+            
+        return guess
